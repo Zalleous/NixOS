1541b47d1f1928d1d21c37a5b309571d4558f109
VOKTEX <74386536+VoktexYT@users.noreply.github.com>
2025-11-30 14:06:47 -0500

    Add additional packages and keybindings in home.nix

--- home.nix
+++ home.nix
@@ -1,36 +1,30 @@
 { config, pkgs, ... }:
 
 {
-  home.packages = with pkgs; [
-    git
+  home.packages = with pkgs; [
+    git
+    foot
+    fuzzel
+    waybar
     
-    foot
-    fuzzel
-    
-    waybar
-  ];
+    firefox
+    qutebrowser
+  ];
 
-  programs.fish = {
-    enable = true;
-  };
-  
-  programs.neovim = {
-    enable = true;
-  };
-
-  programs.sway = {
-    enable = true;
-    config = {
-      terminal = "foot";
-      keybindings = {
-        "Mod4+Return" = "exec foot";
-        "Mod4+Shift+c" = "kill";
-      };
-    };
-  };
-
-  home.sessionVariables = {
-  };
-  
-  home.stateVersion = "25.11";
+  programs.fish.enable = true;
+  programs.neovim.enable = true;
+  
+  programs.sway = {
+    enable = true;
+    config = {
+      terminal = "foot";
+      keybindings = {
+        "Mod4+Return" = "exec foot";
+        "Mod4+Shift+c" = "kill";
+        "Mod4+d" = "exec fuzzel";
+      };
+    };
+  };
+  
+  home.stateVersion = "25.11";
 }
