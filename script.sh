72befaf93e970d82188cd529fb3ffb442d19aff8
VOKTEX <74386536+VoktexYT@users.noreply.github.com>
2025-11-30 08:00:10 -0500

    Add script to install necessary packages

---
+++ script.sh
@@ -0,0 +1,11 @@
+sudo apt update
+sudo apt upgrade -y
+sudo apt install -y git cmake meson ninja-build gcc g++ \
+  libwayland-dev libxkbcommon-dev libinput-dev libdrm-dev \
+  libgbm-dev libpixman-1-dev libseat-dev libudev-dev \
+  libxcb1-dev libxcb-composite0-dev libxcb-dri3-dev \
+  libxcb-ewmh-dev libxcb-icccm4-dev libxcb-present-dev \
+  libxcb-render-util0-dev libxcb-res0-dev libxcb-util-dev \
+  libxcb-xfixes0-dev libxcb-xinput-dev libvulkan-dev \
+  libpango1.0-dev libcairo2-dev hwdata jq xwayland \
+  libtomlplusplus-dev pkg-config
