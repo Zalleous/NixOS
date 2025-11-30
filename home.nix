c16390318d6f0302227a8fd1a733755aa185e0b5
VOKTEX <74386536+VoktexYT@users.noreply.github.com>
2025-11-30 14:18:25 -0500

    Replace swayfx with sway in home.nix configuration

--- home.nix
+++ home.nix
@@ -1,41 +1,42 @@
 { config, pkgs, ... }:
 
 {
-  home.packages = with pkgs; [
-    git
-    foot
-    fuzzel
-    waybar
+  home.packages = with pkgs; [
+    git
+    foot
+    fuzzel
+    waybar
     
     firefox
     qutebrowser
-  ];
+    
+    swayfx 
+  ];
 
   programs.fish = {
-    enable = true;
+    enable = true;
     init = ''
       if status is-login
         if test -z "$DISPLAY" -a -z "$WAYLAND_DISPLAY"
-          exec sway
+          exec swayfx
         end
       end
     '';
-  };
-  
+  };
+  
   programs.neovim.enable = true;
 
-  programs.swayfx = {
-    services.run.sway.enable = true;
-    enable = true;
-    config = {
-      terminal = "foot";
-      keybindings = {
-        "Mod4+Return" = "exec foot";
-        "Mod4+Shift+c" = "kill";
-        "Mod4+d" = "exec fuzzel";
-      };
-    };
-  };
-  
-  home.stateVersion = "24.05";
+  programs.sway = {
+    enable = true;
+    config = {
+      terminal = "foot";
+      keybindings = {
+        "Mod4+Return" = "exec foot";
+        "Mod4+Shift+c" = "kill";
+        "Mod4+d" = "exec fuzzel";
+      };
+    };
+  };
+  
+  home.stateVersion = "24.05";
 }
